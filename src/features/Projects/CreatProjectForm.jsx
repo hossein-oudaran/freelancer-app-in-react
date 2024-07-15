9851639b407fac33092287cfa555a8a9ad11dd5b
hossein-oudaran <hossein.oudaran@gmail.com>
2024-07-15 16:18:05 +0330

    CreactProjectForm component

--- src/features/Projects/CreatProjectForm.jsx
+++ src/features/Projects/CreatProjectForm.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import TextField from "../../ui/TextField";
 
 function CreatProjectForm() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>CreatProjectForm</div>
-  )
+    <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        label="عنوان پروژه"
+        name="title"
+        register={register}
+        required
+        validationSchema={{
+          required: "نوشتن عنوان پروژه ضروری است",
+          minLength: {
+            value: 10,
+            message: "طول عنوان باید بیشتر از 10 کاراکتر باشد",
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="توضیحات"
+        name="description"
+        register={register}
+        required
+        validationSchema={{
+          required: "توضیحات ضروری است",
+          minLength: {
+            value: 15,
+            message: "حداقل 15 کاراکتر را وارد کنید",
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="بودجه"
+        name="budget"
+        type="number"
+        register={register}
+        required
+        validationSchema={{
+          required: "بودجه ضروری است",
+        }}
+        errors={errors}
+      />
+      <button type="submit" className="btn btn--primary w-full">
+        تأیید
+      </button>
+    </form>
+  );
 }
 
-export default CreatProjectForm
+export default CreatProjectForm;
