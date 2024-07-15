eea45d11e2543640e9abfa26f0d75e4821aebbdf
hossein-oudaran <hossein.oudaran@gmail.com>
2024-07-15 14:18:28 +0330

    outSideClick hook

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,9 +1,8 @@
 import http from "./httpService";
 
 export function getOwnerProjectsApi() {
-  return http.get("/project/list").then(({ data }) => data.data);
+  return http.get("/project/owner-projects").then(({ data }) => data.data);
 }
-
 export function removeProjectApi(id) {
   return http.delete(`project/${id}`).then(({ data }) => data.data);
 }
