610f1edcd39c8698d253b73349d8e22e2378c1ad
hossein-oudaran <hossein.oudaran@gmail.com>
2024-09-23 09:34:08 +0330

    develope homePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import UserAvatar from "../features/Authentication/UserAvatar";
+import useUser from "../features/Authentication/useUser";
+import HeaderMenu from "../ui/HeaderMenu";
+
 function Home() {
+  const navigate = useNavigate();
+  const handleSignin = () => {
+    navigate("/auth", { replace: true });
+  };
+
+  const { isLoading } = useUser();
   return (
     <div className="h-screen bg-secondary-0">
-      <div className="container xl:max-w-screen-xl ">
-        <h1 className="p-4 text-xl text-secondary-700">صفحه اصلی</h1>
+      <div className="bg-secondary-0 py-4 px-8 border-b border-secondary-200">
+        <div
+          className={`container xl:max-w-screen-lg flex items-center justify-end gap-x-8
+        ${isLoading ? "blur-sm" : ""}
+        `}
+        >
+          <UserAvatar />
+          <HeaderMenu />
+        </div>
+      </div>
+      <div>
+        <h1 className="font-bold text-center text-2xl md:text-5xl text-secondary-800 my-20">
+          اپلیکیشن فریلنسری
+        </h1>
+
+        <div>
+          <p className="text-center text-secondary-500 text-lg leading-loose">
+            با اپلیکیشن ما، بهترین فریلنسرها را پیدا کنید و پروژه‌های خود را به
+            سرعت و با کیفیت بالا انجام دهید. از پشتیبانی دائمی لذت ببرید و
+            تجربه‌ای بی‌نظیر از همکاری آنلاین داشته باشید. با استفاده از
+            ابزارهای پیشرفته و رابط کاربری ساده، می‌توانید به راحتی پروژه‌های
+            خود را مدیریت کنید و با فریلنسرهای حرفه‌ای در ارتباط باشید. به
+            جامعه‌ای از متخصصان بپیوندید و از امکانات بی‌نظیر ما بهره‌مند شوید
+          </p>
+          <div className="flex justify-center gap-x-8 w-full mt-10">
+            <button onClick={handleSignin} className="btn btn--primary">
+              برای ورود کلیک کنید{" "}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,16 +1,24 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { HiOutlineUser } from "react-icons/hi";
 import DarkModeToggle from "./DarkModeToggle";
 import Logout from "../features/Authentication/Logout";
+import useUser from "../features/Authentication/useUser";
 
 function HeaderMenu() {
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const handleProfileClick = () => {
+    if (user.role === "FREELANCER") return navigate("/freelancer/dashboard");
+    if (user.role === "OWNER") return navigate("/owner/dashboard");
+    if (user.role === "ADMIN") return navigate("/admin/dashboard");
+  };
   return (
     <div>
       <ul className="flex gap-x-4 items-center">
         <li className="flex">
-          <Link to="dashboard">
+          <button onClick={handleProfileClick}>
             <HiOutlineUser className="w-5 h-5 text-primary-900" />
-          </Link>
+          </button>
         </li>
         <li className="flex">
           <DarkModeToggle />
